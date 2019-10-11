6b29fa83e18aa8303d5c3c27c1daf1f11b0bc1ce
Bojo <bozhidar_zahariev@hotmail.com>
2019-10-11 11:48:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -713,3 +713,50 @@ function Frameworks() {
     </div>
   );
 }
+
+//Use Array.filter() to Dynamically Filter an Array
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          username: 'Jeff',
+          online: true
+        },
+        {
+          username: 'Alan',
+          online: false
+        },
+        {
+          username: 'Mary',
+          online: true
+        },
+        {
+          username: 'Jim',
+          online: false
+        },
+        {
+          username: 'Sara',
+          online: true
+        },
+        {
+          username: 'Laura',
+          online: true
+        }
+      ]
+    };
+  }
+  render() {
+    const usersOnline = this.state.users.filter(user => user.online);
+    const renderOnline = usersOnline.map(element => {
+      return <li key={element.username + 1}>{element.username}</li>;
+    });
+    return (
+      <div>
+        <h1>Current Online Users:</h1>
+        <ul>{renderOnline}</ul>
+      </div>
+    );
+  }
+}
