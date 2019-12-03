f552d1d0eb0d1bd404aed14bbcca39144487964e
Bojo <bozhidar_zahariev@hotmail.com>
2019-12-03 15:19:34 +0000

    Update script2.js

--- script2.js
+++ script2.js
@@ -11,3 +11,43 @@ const JSX = (
     </ul>
   </div>
 );
+
+/////////////////////////
+//Add Comments in JSXPassed
+
+const JSX = (
+  <div>
+    {/*comment*/}
+    <h1>This is a block of JSX</h1>
+    <p>Here's a subtitle</p>
+  </div>
+);
+
+////////////////////////////////
+//Render HTML Elements to the DOMPassed
+const JSX = (
+  <div>
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
+  </div>
+);
+// change code below this line
+ReactDOM.render(JSX, document.getElementById('challenge-node'));
+
+///////////////////////////
+//Define an HTML Class in JSX
+const JSX = (
+  <div className="myDiv">
+    <h1 className="something">Add a class to this div</h1>
+  </div>
+);
+
+//////////////////////////////////
+//Create a Stateless Functional Component
+const MyComponent = function() {
+  // change code below this line
+
+  return <div>Hello</div>;
+
+  // change code above this line
+};
