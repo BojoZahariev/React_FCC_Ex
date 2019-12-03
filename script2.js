e291f1a177b133e3a55564b0e80755cce2e6a937
Bojo <bozhidar_zahariev@hotmail.com>
2019-12-03 12:05:18 +0000

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,13 @@
+//React: Create a Complex JSX ElementPassed
 
+const JSX = (
+  <div>
+    <h1></h1>
+    <p></p>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>
+);
