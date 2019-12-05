002802ee21d2c51efd4a761e9c1195c82c53a274
Bojo <bozhidar_zahariev@hotmail.com>
2019-12-05 14:46:50 +0000

    Update script2.js

--- script2.js
+++ script2.js
@@ -448,3 +448,80 @@ class MyComponent extends React.Component {
     );
   }
 }
+
+///////////////////////////////////////////////////
+//Render State in the User Interface Another WayPassed
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'freeCodeCamp'
+    };
+  }
+  render() {
+    // change code below this line
+    const name = this.state.name;
+    // change code above this line
+    return (
+      <div>
+        {/* change code below this line */}
+        <h1>{name}</h1>
+        {/* change code above this line */}
+      </div>
+    );
+  }
+}
+
+/////////////////////////////////////////////////
+//Set State with this.setState
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Initial State'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    // change code below this line
+    this.setState({ name: 'React Rocks!' });
+    // change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+}
+
+////////////////////////////////////////////
+//Bind 'this' to a Class Method
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Hello'
+    };
+    // change code below this line
+    this.handleClick = this.handleClick.bind(this);
+    // change code above this line
+  }
+  handleClick() {
+    this.setState({
+      text: 'You clicked!'
+    });
+  }
+  render() {
+    return (
+      <div>
+        {/* change code below this line */}
+        <button onClick={this.handleClick}>Click Me</button>
+        {/* change code above this line */}
+        <h1>{this.state.text}</h1>
+      </div>
+    );
+  }
+}
